7b5b081f0e742d8d3c957abc1e48c7be4ab1e2c8
emanuel <emanuel.videla.rg@gmail.com>
2023-06-07 19:10:30 -0300

    primera subida

---
+++ 2.5.1 botella metodo recargar/Botella.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5._1_botella_metodo_recargar
+{
+    internal class Botella
+    {
+
+        // agregar metodo o comportamiento "recarga" a botella capacidad maxima 100bcantidad actual..
+        // inicia en cero metodo recarga que carga al 100 y devuelve costo que sera 50 cada 100
+
+        public Botella(string color, string material) // crea clase con parametros definidos
+        {
+            this.color = color;         //recibe
+            this.material = material;   //recibe
+            capacidad = 100;            //creacion defecto- cap max
+            cantidadActual = 0;         //comienza en 0
+
+        }
+
+        private string color, material;
+        private int capacidad, cantidadActual;
+        float monto;
+
+        public int Capacidad
+        {
+            get { return capacidad; }
+        }
+
+        public int CantidadActual
+        {
+            get { return cantidadActual; }
+        }
+        public float Monto
+        {
+            get { return monto; }
+        }
+
+        //metodo recargar
+
+        public float recargar()
+        {
+            cantidadActual = 100;
+            monto = 50;
+            return monto;
+        }
+
+
+    }
+}
---
+++ 2.5.1 botella metodo recargar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5._1_botella_metodo_recargar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Botella b1 = new Botella("Rojo","Vidrio");
+            //b1.Capacidad = 150; no se puede asignar xq nace con una cap max 100 en clase botella
+
+            Console.WriteLine("La capacidad de la botella es" + b1.Capacidad);
+            Console.WriteLine("y el contenido actual es: " + b1.CantidadActual);
+
+            //aplicamos metodo.. comportamiento.. recargamos..
+
+            string ingreso;
+
+            Console.WriteLine("Ingrese 1 si desea llenar la botella, de lo contrario presione cualquier tecla");
+            ingreso = Console.ReadLine();
+
+            if (ingreso == "1"){
+                b1.recargar();
+                Console.WriteLine("Luego de la recarga el contenido actual es: " + b1.CantidadActual);
+                Console.WriteLine("El monto de la recarga es: " + b1.Monto);
+            }else{
+                Console.WriteLine("No se recargo la botella");
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
